8e6c0ddda58f11a0ccb35afc47b37635898cf839
f4exb <f4exb06@free.fr>
2018-06-14 14:32:58 +0200

    NXDN: added a scrambler unit test

--- testfec/pn.cpp
+++ testfec/pn.cpp
@@ -15,8 +15,33 @@
 ///////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <iomanip>
 #include <string.h>
 #include "../pn.h"
+
+unsigned char NXDN_PN_REF[48] = {0x27, 0x2a, 0xc3, 0x7a, 0x6e, 0x45, 0xa, 0xd3, 0xf6, 0x49, 0x6f, 0xc9, 0xa9, 0x98, 0xc, 0x65, 0x1a, 0x5f, 0xd1, 0x63, 0xac, 0xb3, 0xc7, 0xdd, 0x6, 0xb6, 0xec, 0x16, 0xbe, 0xaa, 0x5, 0x2b, 0xcb, 0xb8, 0x1c, 0xe9, 0x3d, 0x75, 0x12, 0x19, 0xc2, 0xf6, 0xcd, 0xe, 0xf0, 0xff, 0x83, 0xdf};
+
+
+void nxdn_scrambler_test()
+{
+    DSDcc::PN_9_5 pn(0xe4);
+
+    //std::cerr << "NXDN scrambler test" << std::endl;
+
+    for (int i = 0; i < 48; i++) {
+        //std::cerr << "0x" << std::hex << (int) pn.getByte(i) << ", ";
+        unsigned char bx = pn.getByte(i);
+        if (bx != NXDN_PN_REF[i])
+        {
+            std::cerr << "NXDN scrambler test failed at index " << i << ": " << std::hex << (int) bx << " != " << (int) NXDN_PN_REF[i] <<  std::endl;
+            return;
+        }
+    }
+    //std::cerr << std::endl;
+
+    std::cerr << "NXDN scrambler test OK" << std::endl;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -46,6 +71,8 @@ int main(int argc, char *argv[])
     {
         std::cerr << "Sequence KO" << std::endl;
     }
+
+    nxdn_scrambler_test();
 }
 
 
